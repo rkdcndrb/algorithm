22f4c091a145521c1a135dd5edc19885dfec2b48
rkdcndrb <kang5858111@naver.com>
2022-07-13 22:06:22 +0900

    백준 9184(동적계획법)

--- Baekjoon9184.java
+++ Baekjoon9184.java
@@ -28,10 +28,10 @@ public class Baekjoon9184 {
     	if(a <= 0 || b <= 0 || c <= 0) return 1;
     	else if(a > 20 || b > 20 || c > 20) return baek9184(20, 20, 20);
     	
-    	if(arr[a][b][c] != 0) return arr[a][b][c];
+    	if(arr[a][b][c] != 0) return arr[a][b][c]; // 저장되어 있는경우 계산없이 바로 리
     	
-    	if(a < b && b < c) {
-    		arr[a][b][c] = baek9184(a, b, c-1) + baek9184(a, b-1, c-1) - baek9184(a, b-1, c); 
+    	if(a < b && b < c) { //처음 오는 경우 값 할
+    		arr[a][b][c] = baek9184(a, b, c-1) + baek9184(a, b-1, c-1) - baek9184(a, b-1, c);   
     	}else {
     		arr[a][b][c] = baek9184(a-1,b,c) + baek9184(a-1,b-1,c)+baek9184(a-1,b,c-1)-baek9184(a-1,b-1,c-1);
     	}
